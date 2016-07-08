0ab0575430c3ebe493a3abc398addce047a01f10
Andrew Truman <andy@killbotlogic.com>
2016-07-08 17:27:46 +1000

    hello

--- PersonalPage/Controllers/HomeController.cs
+++ PersonalPage/Controllers/HomeController.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace PersonalPage.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+
+
+           
+
             return View();
         }
     }
